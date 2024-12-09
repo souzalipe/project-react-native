608958e1232d3906c2e9aecbf676918a00ad1e25
robotinho-silva <fn23886@gmail.com>
2024-12-09 18:23:38 -0300

    feat: changing text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello</ThemedText>
+        <ThemedText type="title">Hell</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
